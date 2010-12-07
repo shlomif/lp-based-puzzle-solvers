01f5e7d2e79cfe73bcb9532bb96f6a70d25fa284
Shlomi Fish <shlomif@iglu.org.il>
2010-12-07 22:21:15 +0200

    Started changing the Kakuro Solver.
    However, I've run into a design problem - how can I indicate that all squares
    of one clue must be different?

--- kakuro-solver/setup.py
+++ kakuro-solver/setup.py
@@ -1,14 +1,14 @@
 from distutils.core import setup
 
-dist_name = 'kakurasu_solver'
+dist_name = 'kakuro_solver'
 
 setup(
     name=dist_name,
-    version='0.2.0',
-    description='Solver for the Kakurasu Riddle Game',
+    version='0.0.1',
+    description='Solver for the Kakuro (Cross Sums) Riddle Game',
     author='Shlomi Fish',
     author_email='shlomif@cpan.org',
-    url='http://www.shlomifish.org/open-source/projects/nikoli-solvers/kakurasu/',
+    url='http://www.shlomifish.org/open-source/projects/nikoli-solvers/kakuro/',
 
     py_modules=[dist_name],
     requires = ['lp_solve'],
